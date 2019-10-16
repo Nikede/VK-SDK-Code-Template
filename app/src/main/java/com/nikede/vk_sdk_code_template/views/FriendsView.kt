8f752fab0e3c082c1d7798b2b0ab210cdd916d36
Nikede <andrewkondratyevs@gmail.com>
2019-10-16 14:13:34 +0300

    Change strategy

--- app/src/main/java/com/nikede/vk_sdk_code_template/views/FriendsView.kt
+++ app/src/main/java/com/nikede/vk_sdk_code_template/views/FriendsView.kt
@@ -2,11 +2,13 @@ package com.nikede.vk_sdk_code_template.views
 
 import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.nikede.vk_sdk_code_template.models.FriendModel
 
 @StateStrategyType(value = AddToEndSingleStrategy::class)
 interface FriendsView:MvpView {
+    @StateStrategyType(value = SingleStateStrategy::class)
     fun showError(textSource: Int)
     fun setupEmptyList()
     fun setupFriendsList(friendsList: ArrayList<FriendModel>)
--- app/src/main/java/com/nikede/vk_sdk_code_template/views/LoginView.kt
+++ app/src/main/java/com/nikede/vk_sdk_code_template/views/LoginView.kt
@@ -2,12 +2,14 @@ package com.nikede.vk_sdk_code_template.views
 
 import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
 @StateStrategyType(value = AddToEndSingleStrategy::class)
 interface LoginView: MvpView {
     fun startLoading()
     fun endLoading()
+    @StateStrategyType(value = SingleStateStrategy::class)
     fun showError(textSource: Int)
     fun openFriends()
 }
